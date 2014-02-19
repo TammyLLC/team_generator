8f023a64d8c8daf5f3c7e463e9d309ea03fb9d06
TammyLLC <me@TammyLLC.com>
2014-02-19 11:41:42 -0500

    Font/BG color and welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 TeamGenerator::Application.routes.draw do
   resources :teams
+  root 'welcome#home'
 
+  get "welcome/home"
   resources :students
 
   # The priority is based upon order of creation: first created -> highest priority.
